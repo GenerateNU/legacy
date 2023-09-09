1d3d8eacd81d38ea1141b95b6a53d8f6214bcdac
David Oduenye <davidoduneye1@gmail.com>
2023-09-08 21:35:22 -0400

    init

--- server/src/main.go
+++ server/src/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"server/src/models"
 )
 
 func main() {
-    result := models.Add(3, 5)
-    fmt.Println("3 + 5 =", result)
-            fmt.Println("Hello, World!")
-            fmt.Println("Hello, World!")
+    // var unusedVar string = "I am not used"
+	fmt.Println("Hello, world!")
 }
