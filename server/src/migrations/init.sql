bf60dd447f9e20ee6b8115594087bbec554188c5
David Oduenye <davidoduneye1@gmail.com>
2023-09-27 11:15:33 -0400

    delete(testfile.txt) removed unncessary file

--- server/src/migrations/init.sql
+++ server/src/migrations/init.sql
@@ -24,13 +24,13 @@ CREATE TABLE IF NOT EXISTS user_profile
     user_id UUID UNIQUE,
     name VARCHAR(255),
     date_of_birth DATE,
-    phone_number VARCHAR(20), -- Adjust the data type for phone numbers as needed
+    phone_number VARCHAR(20), 
     age INT,
-    onboarding_response JSONB, -- Using JSONB for structured data
+    onboarding_response JSONB, 
     FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE task (
+CREATE TABLE IF NOT EXISTS task (
     task_id UUID PRIMARY KEY,
     task_name VARCHAR(255),
     task_description TEXT,
@@ -38,7 +38,7 @@ CREATE TABLE task (
     progress BOOLEAN,
 );
 
-CREATE TABLE subtask (
+CREATE TABLE IF NOT EXISTS subtask (
     sub_task_id UUID PRIMARY KEY,
     task_id UUID,
     task_name VARCHAR(255),
@@ -48,16 +48,37 @@ CREATE TABLE subtask (
     FOREIGN KEY (task_id) REFERENCES task (task_id)
 );
 
-CREATE TABLE persona (
+CREATE TABLE IF NOT EXISTS persona (
     persona_id UUID PRIMARY KEY,
     persona_description TEXT,
     persona_title VARCHAR(255),
 );
 
-CREATE TABLE persona_task (
+CREATE TABLE IF NOT EXISTS persona_task (
     persona_id UUID,
     task_id UUID,
     PRIMARY KEY (persona_id, task_id),
     FOREIGN KEY (persona_id) REFERENCES persona (persona_id),
     FOREIGN KEY (task_id) REFERENCES task (task_id)
 );
+
+-- Files Table connected to the s3 bucket
+CREATE TABLE IF NOT EXISTS files (
+    file_id UUID PRIMARY KEY,
+    file_name VARCHAR(255),
+    file_description TEXT,
+    file_path VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    user_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+-- Progress Table
+CREATE TABLE IF NOT EXISTS progress (
+    progress_id UUID PRIMARY KEY,
+    user_id UUID,
+    task_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    FOREIGN KEY (task_id) REFERENCES task (task_id)
+); 
