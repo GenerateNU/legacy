43f7d9974d08c2ff1d0d87ef12e40959c5010efe
David Oduenye <davidoduneye1@gmail.com>
2023-10-03 11:21:41 -0400

    ci(CI.yaml): added script for actions tty mode

--- scripts/verify_os.sh
+++ scripts/verify_os.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
-if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "darwin"* ]]; then
-    echo "Linux or Mac OS detected"
-    ./init_db.sh
-elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
-    echo "Windows detected"
-    ./init_db_windows.sh
-elif [ -n "$(command -v wsl)" ]; then
-    echo "WSL detected"
-    ./init_db.sh
-else
-    echo "Unsupported operating system"
-    exit 1
+if [ -t 0 ]; then
+    if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "darwin"* ]]; then
+        echo "Linux or Mac OS detected"
+        ./init_db.sh
+    elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+        echo "Windows detected"
+        ./init_db_windows.sh
+    elif [ -n "$(command -v wsl)" ]; then
+        echo "WSL detected"
+        ./init_db.sh
+    else
+        echo "Unsupported operating system"
+        exit 1
+    fi
+else 
+    echo "Github Action detected"
+    ./init_db_non_interactive.sh
 fi
