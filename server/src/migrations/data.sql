656dfa481e8de740f029460f3ac4a9f02bbb6727
melodyyu754 <118621363+melodyyu754@users.noreply.github.com>
2023-10-09 16:37:26 -0400

    Inserting example tasks to db

---
+++ server/src/migrations/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO tasks (task_name, task_description)  VALUES
+('Fear of end of life planning', 'Acknowledge your aversion to end of life planning'),
+('Understanding EOLP', 'Create familiarity with the process'),
+('Values and Priorities', 'Define your values and priorities'),
+('Initial Plan', 'Create an initial end-of-life plan'),
+
+('Existing Financials', 'Review your existing financial plan'),
+('Non-financial Aspects', 'Explore non-financial aspects'),
+('Will', 'Create a comprehensive will'),
+('Specific Wishes', 'Communicate any specific wishes'),
+('Plan Moving Forward', 'Regularly review and update'),
+
+('Assemble Team', 'Assemble your financial team'),
+('Living Trust', 'Establish a revocable living trust'),
+('Tax Planning', 'Leverage tax planning strategies'),
+('Family Governance', 'Develop a family governance plan.'),
+('Emotional and Spiritual', 'Address emotional and spiritual concerns.'),
+('Create Project', 'Create a legacy project')
