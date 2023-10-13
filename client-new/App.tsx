abec08ab2c5a7ad0a91943315c780389c58d6fc3
akshayd2020 <akshayd2020@gmail.com>
2023-10-13 16:57:38 -0400

    Temp commit

--- client-new/App.tsx
+++ client-new/App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import Router from "./src/navigation/Router";
 import { NativeBaseProvider, extendTheme } from "native-base";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Auth0Provider } from "react-native-auth0";
 
 export default function App() {
   const theme = extendTheme({
@@ -10,11 +11,16 @@ export default function App() {
     colors: {},
   });
   return (
-    <SafeAreaProvider>
+    <Auth0Provider 
+    domain={"dev-145d20j331samjak.us.auth0.com"}
+    clientId={"0tAv4FM6AkiCPREr9MgbT13aiV5rUVSq"}>
+      <SafeAreaProvider>
       <NativeBaseProvider>
         <Router />
       </NativeBaseProvider>
     </SafeAreaProvider>
+    </Auth0Provider>
+    
   );
 }
 
--- client-new/src/screens/auth/AccessScreen.tsx
+++ client-new/src/screens/auth/AccessScreen.tsx
@@ -1,10 +1,24 @@
+import { Button } from "native-base";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth0 } from "react-native-auth0";
+import { Alert } from "react-native";
 
 export default function AccessScreen() {
+  const { authorize } = useAuth0();
+
+  const onPress = async () => {
+    try {
+      await authorize();
+      Alert.alert("Success", "You have successfully logged in");
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Access Screen, Welcome to Legacy! This is Akshay</Text>
+      <Button onPress={onPress}></Button>
     </SafeAreaView>
   );
 }
