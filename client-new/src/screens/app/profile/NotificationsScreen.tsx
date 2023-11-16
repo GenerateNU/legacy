c8a53e0e8343089c7911ef5b337c18bf2b8ebdb3
Dave Budhram <davebudhram18@gmail.com>
2023-11-16 12:18:00 -0500

    finished notifications screen

--- client-new/src/screens/app/profile/NotificationsScreen.tsx
+++ client-new/src/screens/app/profile/NotificationsScreen.tsx
@@ -1,12 +1,27 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import React from "react";
+import React, {useState} from "react";
 import LegacyWordmarkWithBackArrow from "../../../components/reusable/LegacyWordmarkWithBackArrow";
 import {View, Text} from "native-base";
+import NotificationsToggle from "../../../components/reusable/NotificationsToggle";
 
+// Currently does actually set reminders or store user preferecnes in local storage
 export default function NotificationsScreen({route, navigation}) {
+  const [motivationReminders, setMotivationReminders] =
+    useState<boolean>(false);
+  const [progressReminders, setProgressReminders] = useState<boolean>(false);
+
   const toProfile = () => {
     navigation.navigate("Profile Screen");
   };
+
+  const handleMotivationReminders = (newValue: boolean) => {
+    setMotivationReminders(newValue);
+  };
+
+  const handleProgressReminders = (newValue: boolean) => {
+    setProgressReminders(newValue);
+  };
+
   return (
     <SafeAreaView style={{alignItems: "center", flex: 1}}>
       <View width={340} marginTop={50} height={"auto"}>
@@ -18,10 +33,21 @@ export default function NotificationsScreen({route, navigation}) {
           fontWeight={"700"}
           lineHeight={20}
           marginTop={18}
-          marginBottom={16}
+          marginBottom={10}
         >
           Notifications
         </Text>
+
+        <NotificationsToggle
+          title='Allow motivation reminders'
+          toggle={motivationReminders}
+          toggleChange={handleMotivationReminders}
+        />
+        <NotificationsToggle
+          title='Allow progress reminders'
+          toggle={progressReminders}
+          toggleChange={handleProgressReminders}
+        />
       </View>
     </SafeAreaView>
   );
